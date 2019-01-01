feefefdaa232720381d075c37e4d739aa9d686cb
Cesar Ferreira <cesar.manuel.ferreira@gmail.com>
2019-01-01 11:46:17 +0000

    remove test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-import test from 'ava';
-import execa from 'execa';
+// import test from 'ava';
+// import execa from 'execa';
 
-test(async t => {
-	const {stdout} = await execa('./cli.js', ['--version']);
-	t.true(stdout.length > 0);
-});
+// test(async t => {
+// 	const {stdout} = await execa('./cli.js', ['--version']);
+// 	t.true(stdout.length > 0);
+// });
